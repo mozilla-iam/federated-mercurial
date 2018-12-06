82656e06e03507ebf19653e4a7da715431b41074
Jeff Bryner <jeff@jeffbryner.com>
2018-12-06 10:27:45 -0500

    pseudo code commit

--- hgext/client/federated_auth.py
+++ hgext/client/federated_auth.py
@@ -10,8 +10,23 @@ cmdtable = {}
 command = cmdutil.command(cmdtable)
 
 testedwith = '2.2 2.3'
+def retrieve_well_known(well_known_url):
+    resp = requests.get(well_known_url)
+    data = resp.json()
+    return data['token_endpoint'], data['authorization_endpoint']
+
 
 @command('oidcauth',[])
 def oidcauth(ui, repo, node, **opts):
     #ctx = repo[node]
-    ui.write(_('hello world\n'))
+    ui.write(_('here we go!\n'))
+
+    # load up config
+
+    # init login flow
+
+    # get the response
+
+    # post the response to kang
+
+
